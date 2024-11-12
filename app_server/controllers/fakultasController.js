43ebf69617d360dc5a96487502a09d97aaa5b7d8
nurrachmat <rachmat.nur@gmail.com>
2024-11-12 09:39:52 +0700

    Store Fakultas

--- app_server/controllers/fakultasController.js
+++ app_server/controllers/fakultasController.js
@@ -24,4 +24,28 @@ const index = async (req, res) => {
   }
 };
 
-module.exports = { index };
+const store = async (req, res) => {
+  const { nama, singkatan } = req.body;
+  try {
+    const response = await fetch(
+      "https://crud-express-seven.vercel.app/api/fakultas",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nama, singkatan }),
+      }
+    );
+
+    if (response.ok) {
+      res.redirect("/fakultas"); // Redirect ke halaman fakultas setelah berhasil menambah
+    } else {
+      res.status(500).send("Gagal menambahkan data fakultas.");
+    }
+  } catch (error) {
+    res.status(500).send("Error menambahkan data fakultas");
+  }
+};
+
+module.exports = { index, store };
--- app_server/routes/fakultas.js
+++ app_server/routes/fakultas.js
@@ -7,4 +7,7 @@ const fakultasController = require("../controllers/fakultasController");
 /* GET list fakultas. */
 router.get("/", fakultasController.index);
 
+// Route untuk menambahkan data fakultas
+router.post("/store", fakultasController.store);
+
 module.exports = router;
