548daf5ffea3d10065d5726bde993a769aef5313
nurrachmat <rachmat.nur@gmail.com>
2024-11-22 05:32:15 +0700

    Hapus authMiddleware pada method get

--- app_api/routes/fakultas.js
+++ app_api/routes/fakultas.js
@@ -11,15 +11,30 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 
 // Definisi rute untuk fakultas
 // Mengatur rute GET untuk mendapatkan semua data fakultas
-router.get("/", authMiddleware, fakultasController.getAllFakultas);
+router.get("/", fakultasController.getAllFakultas);
 // Mengatur rute POST untuk membuat data fakultas baru
-router.post("/", authMiddleware, roleMiddleware("admin"), fakultasController.createFakultas);
+router.post(
+  "/",
+  authMiddleware,
+  roleMiddleware("admin"),
+  fakultasController.createFakultas
+);
 // Mengatur rute GET untuk mendapatkan data fakultas berdasarkan ID
-router.get("/:id", authMiddleware, fakultasController.getFakultasById);
+router.get("/:id", fakultasController.getFakultasById);
 // Mengatur rute PUT untuk memperbarui data fakultas berdasarkan ID
-router.put("/:id", authMiddleware, roleMiddleware("admin"), fakultasController.updateFakultas);
+router.put(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("admin"),
+  fakultasController.updateFakultas
+);
 // Mengatur rute DELETE untuk menghapus data fakultas berdasarkan ID
-router.delete("/:id", authMiddleware, roleMiddleware("admin"), fakultasController.deleteFakultas);
+router.delete(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("admin"),
+  fakultasController.deleteFakultas
+);
 
 // Mengeksport router agar dapat digunakan di file lain (misalnya, di app.js)
 module.exports = router;
--- app_api/routes/mahasiswa.js
+++ app_api/routes/mahasiswa.js
@@ -8,23 +8,40 @@ const authMiddleware = require("../middleware/authMiddleware");
 const roleMiddleware = require("../middleware/roleMiddleware");
 
 // Route untuk mendapatkan semua data mahasiswa
-router.get("/", authMiddleware, mahasiswaController.getAllMahasiswa);
+router.get("/", mahasiswaController.getAllMahasiswa);
 // Mengarahkan ke fungsi getAllMahasiswa di controller untuk mengambil semua data mahasiswa
 
 // Route untuk mendapatkan data mahasiswa berdasarkan ID
-router.get("/:id", authMiddleware, mahasiswaController.getMahasiswaById);
+router.get("/:id", mahasiswaController.getMahasiswaById);
 // Mengarahkan ke fungsi getMahasiswaById di controller untuk mengambil data mahasiswa berdasarkan ID
 
 // Route untuk menambahkan data mahasiswa baru, termasuk upload file foto
-router.post("/", authMiddleware, roleMiddleware("admin"), upload.single("foto"), mahasiswaController.createMahasiswa);
+router.post(
+  "/",
+  authMiddleware,
+  roleMiddleware("admin"),
+  upload.single("foto"),
+  mahasiswaController.createMahasiswa
+);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi createMahasiswa dijalankan
 
 // Route untuk memperbarui data mahasiswa, dengan upload foto opsional
-router.put("/:id", authMiddleware, roleMiddleware("admin"), upload.single("foto"), mahasiswaController.updateMahasiswa);
+router.put(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("admin"),
+  upload.single("foto"),
+  mahasiswaController.updateMahasiswa
+);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi updateMahasiswa dijalankan
 
 // Route untuk menghapus data mahasiswa berdasarkan ID
-router.delete("/:id", authMiddleware, roleMiddleware("admin"), mahasiswaController.deleteMahasiswa);
+router.delete(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("admin"),
+  mahasiswaController.deleteMahasiswa
+);
 // Mengarahkan ke fungsi deleteMahasiswa di controller untuk menghapus data mahasiswa berdasarkan ID
 
 module.exports = router; // Mengekspor router agar dapat digunakan di file lain
