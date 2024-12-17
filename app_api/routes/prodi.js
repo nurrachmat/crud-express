b0e79bc2e8e3a7ffa0bca3d1d001f9f28e65917c
nurrachmat <rachmat.nur@gmail.com>
2024-12-17 10:10:22 +0700

    Update prodi.js

--- app_api/routes/prodi.js
+++ app_api/routes/prodi.js
@@ -13,11 +13,21 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 // Mengatur rute GET untuk mendapatkan semua data prodi
 router.get("/", prodiController.getAllProdi);
 // Mengatur rute POST untuk membuat data prodi baru
-router.post("/", prodiController.createProdi);
+router.post(
+  "/",
+  authMiddleware,
+  roleMiddleware("admin"),
+  prodiController.createProdi
+);
 // Mengatur rute GET untuk mendapatkan data prodi berdasarkan ID
 router.get("/:id", prodiController.getProdiById);
 // Mengatur rute PUT untuk memperbarui data prodi berdasarkan ID
-router.put("/:id", prodiController.updateProdi);
+router.put(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("admin"),
+  prodiController.updateProdi
+);
 // Mengatur rute DELETE untuk menghapus data prodi berdasarkan ID
 router.delete(
   "/:id",
