8e1c5c06e759259caa7b65b875479cb824e42e96
nurrachmat <rachmat.nur@gmail.com>
2024-12-17 10:03:11 +0700

    Update prodi.js

--- app_api/routes/prodi.js
+++ app_api/routes/prodi.js
@@ -5,6 +5,10 @@ const router = express.Router();
 // Mengimpor Controller prodi untuk menangani logika bisnis
 const prodiController = require("../controllers/prodiController");
 
+// Mengimpor middleware untuk autentikasi dan pengecekan peran
+const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
+
 // Definisi rute untuk prodi
 // Mengatur rute GET untuk mendapatkan semua data prodi
 router.get("/", prodiController.getAllProdi);
@@ -15,7 +19,12 @@ router.get("/:id", prodiController.getProdiById);
 // Mengatur rute PUT untuk memperbarui data prodi berdasarkan ID
 router.put("/:id", prodiController.updateProdi);
 // Mengatur rute DELETE untuk menghapus data prodi berdasarkan ID
-router.delete("/:id", prodiController.deleteProdi);
+router.delete(
+  "/:id",
+  authMiddleware,
+  roleMiddleware("admin"),
+  prodiController.deleteProdi
+);
 
 // Mengeksport router agar dapat digunakan di file lain (misalnya, di app.js)
 module.exports = router;
